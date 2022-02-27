3fcd3385cd288411ef58a4e805b1a48ece45cbf9
MCFrank16 <boonatechnologies@gmail.com>
2022-02-27 16:52:59 +0200

    finishing the projects

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,4 +1,4 @@
-var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+var ERC721MintableComplete = artifacts.require('CustomERC721Token');
 
 contract('TestERC721Mintable', accounts => {
 
@@ -6,41 +6,71 @@ contract('TestERC721Mintable', accounts => {
     const account_two = accounts[1];
 
     describe('match erc721 spec', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
+        beforeEach(async function () {
+            this.contract = await ERC721MintableComplete.new({ from: account_one });
 
             // TODO: mint multiple tokens
+            await this.contract.mint(account_two, 4, { from: account_one });
+            await this.contract.mint(account_one, 5, { from: account_one });
+            await this.contract.mint(account_two, 6, { from: account_one });
+            await this.contract.mint(account_one, 7, { from: account_one });
+            await this.contract.mint(account_two, 8, { from: account_one });
+            await this.contract.mint(account_two, 9, { from: account_one });
+            await this.contract.mint(account_one, 10, { from: account_one });
+
         })
 
-        it('should return total supply', async function () { 
-            
+        it('should return total supply', async function () {
+            let result = await this.contract.totalSupply.call();
+            assert.equal(result, 7, "Tokens present not as expected!");
         })
 
-        it('should get token balance', async function () { 
-            
+        it('should get token balance', async function () {
+            let res = await this.contract.balanceOf.call(account_two, { from: account_two })
+            assert.equal(res, 4, "The balance is not the same as expected");
         })
 
-        // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
-        it('should return token uri', async function () { 
-            
+        // // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
+        it('should return token uri', async function () {
+            let res = await this.contract.tokenURI.call(5, { from: account_two })
+            assert.equal(
+                res,
+                "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/5",
+                "Token URI is not as expected!"
+            );
         })
 
-        it('should transfer token from one owner to another', async function () { 
-            
+        it('should transfer token from one owner to another', async function () {
+            await this.contract.transferFrom(account_two, account_one, 4, {
+                from: account_two,
+            });
+
+            const res = await this.contract.ownerOf.call(4, { from: account_one })
+
+            assert(res, account_one, 'The address provided is not the owner of the token')
         })
     });
 
     describe('have ownership properties', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
+        beforeEach(async function () {
+            this.contract = await ERC721MintableComplete.new({ from: account_one });
         })
 
-        it('should fail when minting when address is not contract owner', async function () { 
-            
+        it('should fail when minting when address is not contract owner', async function () {
+            let error;
+            try {
+                await this.contract.mint.call(account_two, 13, { from: account_two });
+            } catch (err) {
+                error = err;
+            }
+
+            assert(error, "Function should only be called by the owner")
         })
 
-        it('should return contract owner', async function () { 
-            
+        it('should return contract owner', async function () {
+            let res = await this.contract.getContractOwner.call({ from: account_two })
+
+            assert(res, account_one, "The contract address returned is not the owner")
         })
 
     });
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,72 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+var SolnSquareVerifier = artifacts.require("SolnSquareVerifier")
+var verifierContract = artifacts.require("Verifier")
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+const proofFromFile = require("../../zokrates/code/square/proof.json");
+
+const getProofAsUint = (proofCheck) => {
+    return {
+        "proof": {
+            "a": [web3.utils.toBN(proofCheck.proof.a[0]).toString(), web3.utils.toBN(proofCheck.proof.a[1]).toString()],
+            "b": [[web3.utils.toBN(proofCheck.proof.b[0][0]).toString(), web3.utils.toBN(proofCheck.proof.b[0][1]).toString()],
+            [web3.utils.toBN(proofCheck.proof.b[1][0]).toString(), web3.utils.toBN(proofCheck.proof.b[1][1]).toString()]
+            ],
+            "c": [web3.utils.toBN(proofCheck.proof.c[0]).toString(), web3.utils.toBN(proofCheck.proof.c[1]).toString()],
+        },
+        "inputs": proofCheck.inputs
+    };
+};
+
+contract("TestSolnSquareVerifier", (accounts) => {
+
+    const account_one = accounts[0]
+
+    describe("Check if the solutions can be added", () => {
+        beforeEach(async () => {
+            this.Verifier = await verifierContract.new({ from: account_one });
+            this.SolnSquareVerifier = await SolnSquareVerifier.new(this.Verifier.address, { from: account_one })
+        })
+
+        // Test if a new solution can be added for contract - SolnSquareVerifier
+        it("test if a new solution can be added for contract", async () => {
+            const proofCheck = getProofAsUint(proofFromFile)
+
+            await this.SolnSquareVerifier.addSolution.call(
+                index = 1,
+                account_one,
+                proofCheck.proof.a,
+                proofCheck.proof.b,
+                proofCheck.proof.c,
+                proofCheck.inputs,
+                { from: account_one }
+            );
+
+            const countSolutions = await this.SolnSquareVerifier.getSolutionCount.call({ from: account_one });
+            
+            assert(countSolutions, 1, 'The solution has not been added');
+        })
+
+        // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+        it(" test if an ERC721 token can be minted", async () => {
+            const proofCheck = getProofAsUint(proofFromFile)
+
+            let res = await this.SolnSquareVerifier.mintToken.call(
+                proofCheck.proof.a,
+                proofCheck.proof.b,
+                proofCheck.proof.c,
+                proofCheck.inputs,
+                ID = 4,
+                { from: account_one }
+            )
+
+            assert.equal(res, true, 'The token was not minted')
+        })
+    })
+
+
+
+
+
+    
+})
+
+
